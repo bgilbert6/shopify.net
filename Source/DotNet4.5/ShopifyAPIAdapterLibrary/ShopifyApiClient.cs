1a9d9c7abdbafd9d7791df7b1cb37ea89b536dbd
bgilbert6 <bgilbert@w3trends.com>
2020-08-10 10:29:13 -0500

    @ work old changes

--- Source/DotNet4.5/ShopifyAPIAdapterLibrary/ShopifyApiClient.cs
+++ Source/DotNet4.5/ShopifyAPIAdapterLibrary/ShopifyApiClient.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.Specialized;
+using System.Configuration;
 using System.IO;
+using System.Linq;
 using System.Net;
 using System.Text;
+using System.Threading;
 using System.Web;
 
 
@@ -18,6 +21,8 @@ namespace ShopifyAPIAdapterLibrary
     /// <seealso cref="http://api.shopify.com/"/>
     public class ShopifyAPIClient
     {
+        public static string ApiVersion = "";
+
         /// <summary>
         /// Creates an instance of this class for use with making API Calls
         /// </summary>
@@ -47,7 +52,7 @@ namespace ShopifyAPIAdapterLibrary
         /// <returns>the server response</returns>
         public object Call(HttpMethods method, string path)
         {
-            return Call(method, path, null);
+            return Call(method, path, null, 0);
         }
 
         /// <summary>
@@ -58,13 +63,32 @@ namespace ShopifyAPIAdapterLibrary
         /// <param name="callParams">any parameters needed or expected by the API</param>
         /// <seealso cref="http://api.shopify.com/"/>
         /// <returns>the server response</returns>
-        public object Call(HttpMethods method, string path, object callParams)
+        public object Call(HttpMethods method, string path, object callParams, int retryCount)
         {
+            var webConfigApiVersion = ConfigurationManager.AppSettings["ShopifyApiVersion"];
+
+            if(string.IsNullOrEmpty(ApiVersion))
+            {
+                if (!string.IsNullOrEmpty(webConfigApiVersion))
+                {
+                    ApiVersion = webConfigApiVersion;
+                }
+            }
+
+
+            if(!string.IsNullOrEmpty(ApiVersion) && !path.Contains("/api/") && !path.Contains("/oauth/")) {
+                path = path.Replace("/admin/", "/admin/api/" + ApiVersion + "/");
+            }
+
+            //reset api version
+            ApiVersion = "";
+
             string url = String.Format("https://{0}.myshopify.com{1}", State.ShopName, path);
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
             request.ContentType = GetRequestContentType();
             request.Headers.Add("X-Shopify-Access-Token", this.State.AccessToken);
             request.Method = method.ToString();
+
             if (callParams != null)
             {
                 if (method == HttpMethods.GET || method == HttpMethods.DELETE)
@@ -108,24 +132,125 @@ namespace ShopifyAPIAdapterLibrary
                 }
             }
 
-            var response = (HttpWebResponse)request.GetResponse();
-            string result = null;
-
-            using (Stream stream = response.GetResponseStream())
+            try
             {
-                StreamReader sr = new StreamReader(stream);
-                result = sr.ReadToEnd();
-                sr.Close();
+
+            
+
+                var response = (HttpWebResponse)request.GetResponse();
+                string result = null;
+                
+                string limit = response.GetResponseHeader("HTTP_X_SHOPIFY_SHOP_API_CALL_LIMIT");
+                HttpContext.Current.Items["HTTP_X_SHOPIFY_SHOP_API_CALL_LIMIT"] = limit;
+
+                double percent = Convert.ToDouble(limit.Split('/')[0]) / Convert.ToDouble(limit.Split('/')[1]);
+
+                //cursor paging
+                string linkHeader = response.GetResponseHeader("LINK");
+                
+                
+                HttpContext.Current.Items["PAGING_LINK"] = "";
+                HttpContext.Current.Items["PAGING_LINK_PREV"] = "";
+
+                if (!string.IsNullOrEmpty(linkHeader))
+                {
+                    var relNext = linkHeader.Split(',').ToList().Where(x => x.Contains("rel=\"next\"")).FirstOrDefault();
+
+                    if(!string.IsNullOrEmpty(relNext))
+                    {
+                        var tempUrl = relNext.Trim().Split('>')[0].Replace("<", "");
+                        
+                        string pageInfo = HttpUtility.ParseQueryString(new Uri(tempUrl).Query)["page_info"];
+
+                        if(!string.IsNullOrEmpty(pageInfo))
+                        {
+                            HttpContext.Current.Items["PAGING_LINK"] = pageInfo;
+                        }
+                        
+                    }
+
+                    var relPrev = linkHeader.Split(',').ToList().Where(x => x.Contains("rel=\"previous\"")).FirstOrDefault();
+
+                    if (!string.IsNullOrEmpty(relPrev))
+                    {
+                        var tempUrl = relPrev.Trim().Split('>')[0].Replace("<", "");
+
+                        string pageInfo = HttpUtility.ParseQueryString(new Uri(tempUrl).Query)["page_info"];
+
+                        if (!string.IsNullOrEmpty(pageInfo))
+                        {
+                            HttpContext.Current.Items["PAGING_LINK_PREV"] = pageInfo;
+                        }
+
+                    }
+
+
+                }
+                //end cursor paging
+
+
+                //WriteToLog(HttpContext.Current.Request.ServerVariables["REMOTE_ADDR"] + ", " + ((int)response.StatusCode).ToString() + ", " + method + ", " + path + " Percent: " + percent + ", HTTP_X_SHOPIFY_SHOP_API_CALL_LIMIT:" + response.GetResponseHeader("HTTP_X_SHOPIFY_SHOP_API_CALL_LIMIT"));
+
+                using (Stream stream = response.GetResponseStream())
+                {
+                    StreamReader sr = new StreamReader(stream);
+                    result = sr.ReadToEnd();
+                    sr.Close();
+                }
+                                
+                //At least one endpoint will return an empty string, that we need to account for.
+                if (string.IsNullOrWhiteSpace(result))
+                    return null;
+
+                if (Translator != null)
+                    return Translator.Decode(result);
+
+                return result;
+            }
+            catch (WebException ex)
+            {
+                var response = ((HttpWebResponse)ex.Response);
+
+                if((int)response.StatusCode == 429)
+                {
+                    double retryAfter = Convert.ToDouble(response.GetResponseHeader("Retry-After"));
+
+                    Thread.Sleep(Convert.ToInt32(retryAfter * 1000));
+
+                    return Call(method, path, callParams, retryCount + 1);
+                }
+
+                
+                
+                WriteToLog(((int)response.StatusCode).ToString() + ", " + method + ", " + path + " HTTP_X_SHOPIFY_SHOP_API_CALL_LIMIT:" + response.GetResponseHeader("HTTP_X_SHOPIFY_SHOP_API_CALL_LIMIT") + ", Shop: " + State.ShopName);
+
+                string result = "";
+
+                using (Stream stream = response.GetResponseStream())
+                {
+                    StreamReader sr = new StreamReader(stream);
+                    result = sr.ReadToEnd();
+                    sr.Close();
+                }
+
+                //apparently shopify can respond with 500 after timing out
+                //dont retry more than 5 times. don't want to enter recursive loop in case the 500 error is unrelated to timeout
+                //https://community.shopify.com/c/Shopify-APIs-SDKs/500-quot-errors-quot-quot-Internal-Server-Error-quot-from-simple/td-p/441380
+                if ((int)response.StatusCode == 500 && retryCount <= 5)
+                {
+                    
+                    //wait 3 seconds
+                    Thread.Sleep(Convert.ToInt32(5000));
+
+                    return Call(method, path, callParams, retryCount + 1);
+                }
+
+                WriteToLog(result);
+
+                throw ex;
             }
 
-            //At least one endpoint will return an empty string, that we need to account for.
-            if (string.IsNullOrWhiteSpace(result))
-                return null;
-
-            if (Translator != null)
-                return Translator.Decode(result);
-
-            return result;
+            //throw new Exception(response)
 
         }
 
@@ -149,7 +274,7 @@ namespace ShopifyAPIAdapterLibrary
         /// <returns>the server response</returns>
         public object Get(string path, NameValueCollection callParams)
         {
-            return Call(HttpMethods.GET, path, callParams);
+            return Call(HttpMethods.GET, path, callParams, 0);
         }
 
         /// <summary>
@@ -161,7 +286,7 @@ namespace ShopifyAPIAdapterLibrary
         /// <returns>the server response</returns>
         public object Post(string path, object data)
         {
-            return Call(HttpMethods.POST, path, data);
+            return Call(HttpMethods.POST, path, data, 0);
         }
 
         /// <summary>
@@ -173,7 +298,7 @@ namespace ShopifyAPIAdapterLibrary
         /// <returns>the server response</returns>
         public object Put(string path, object data)
         {
-            return Call(HttpMethods.PUT, path, data);
+            return Call(HttpMethods.PUT, path, data, 0);
         }
 
         /// <summary>
@@ -227,6 +352,13 @@ namespace ShopifyAPIAdapterLibrary
         /// that consumes this class much more clean
         /// </example>
         protected IDataTranslator Translator { get; set; }
+
+        private void WriteToLog(string info)
+        {
+            string file = HttpContext.Current.Server.MapPath("~/apilog.txt");
+
+            System.IO.File.AppendAllText(file, info + Environment.NewLine);
+        }
     }
 
 
